4e31328611bc1a5c71e605e39f0972ba760010ef
AndreyAndreyI <117981137+AndreyAndreyI@users.noreply.github.com>
2023-02-07 14:23:21 +0200

    hw 06 js

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const btnDecrement = document.querySelector('button[data-action=decrement]');
+const btnIncrement = document.querySelector('button[data-action=increment]');
+const counterValue = document.querySelector('#value');
+
+const decreaseValue = () => {
+	counterValue.textContent = Number(counterValue.textContent) - 1;
+}
+
+const increaseValue = () => {
+	counterValue.textContent = Number(counterValue.textContent) + 1;
+}
+
+btnDecrement.addEventListener("click", decreaseValue);
+btnIncrement.addEventListener("click", increaseValue);
